6929e0d33eb8d2e330fe15b0e9f342f02ada9abf
Behruz12390 <168063527+Behruz12390@users.noreply.github.com>
2024-07-30 12:00:00 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+let body = document.querySelector('body');
+let text = document.querySelector('p');
+let a = prompt('Enter body background color')
+let b = prompt('Enter text color')
+body.style.backgroundColor = a
+text.style.color = b
